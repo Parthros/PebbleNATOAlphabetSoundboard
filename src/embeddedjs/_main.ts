67e0087a0f4baa53d8047988ecc6f3d14e81f8ae
fpdutra <fpdutra26@gmail.com>
2026-06-01 21:55:56 -0500

    Adding TS support

--- src/embeddedjs/_main.ts
+++ src/embeddedjs/_main.ts
@@ -1,17 +1,3 @@
-/* ORIGINAL Template
-import Button from "pebble/button"
-
-console.log("hello, button - press a button");
-console.log("  press and hold back to exit");
-
-new Button({
-	types: ["select", "up", "down", "back"],
-	onPush(down, type) {
-		console.log(`${down ? "press" : "release"} ${type}`);
-	}
-});
-*/
-
 // src/embeddedjs/file.ts
 import Poco from "commodetto/Poco";
 import Button from "pebble/button";
